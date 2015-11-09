f6c7a61d51138df46506cee0068991952bff3cba
FPierre <pflauder@gmail.com>
2015-11-09 19:43:56 +0100

    Formulaire de new Person et ajustements SASS

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,20 @@
+class PeopleController < ApplicationController
+  def show
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def udpate
+  end
+
+  def destroy
+  end
+end
