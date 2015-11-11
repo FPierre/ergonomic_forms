5e7eff2c2a48996fdb8f0443c3d11d8f5c5df79c
FPierre <pflauder@gmail.com>
2015-11-11 12:04:36 +0100

    Routes pour Order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :people, param: :public_id
+  with_options param: :public_id do |route|
+    route.resources :people
+    route.resources :orders
+
+    route.resources :car do
+      route.resources :logbooks
+    end
+  end
 
   root 'people#new'
 end
