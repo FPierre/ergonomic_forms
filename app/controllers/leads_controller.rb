689244e29c8c7941ea6d045798587efcbd359828
FPierre <pflauder@gmail.com>
2016-10-01 15:55:47 +0200

    Suppression commentaires générés

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,25 +1,20 @@
 class LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :edit, :update, :destroy]
 
-  # GET /leads
   def index
     @leads = Lead.all
   end
 
-  # GET /leads/1
   def show
   end
 
-  # GET /leads/new
   def new
     @lead = Lead.new
   end
 
-  # GET /leads/1/edit
   def edit
   end
 
-  # POST /leads
   def create
     @lead = Lead.new(lead_params)
 
@@ -30,7 +25,6 @@ class LeadsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /leads/1
   def update
     if @lead.update(lead_params)
       redirect_to @lead, notice: 'Lead was successfully updated.'
@@ -39,20 +33,18 @@ class LeadsController < ApplicationController
     end
   end
 
-  # DELETE /leads/1
   def destroy
     @lead.destroy
     redirect_to leads_url, notice: 'Lead was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lead
-      @lead = Lead.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def lead_params
-      params.require(:lead).permit(:firstname, :lastname, :thoroughfare, :city, :postal_code, :country, :birth_date)
-    end
+  def set_lead
+    @lead = Lead.find(params[:id])
+  end
+
+  def lead_params
+    params.require(:lead).permit(:firstname, :lastname, :thoroughfare, :city, :postal_code, :country, :birth_date)
+  end
 end
